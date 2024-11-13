d67d1a441449761b521c69ba0a015fda7d6ac057
34-43 <jehyuck3443@gmail.com>
2024-11-13 18:01:45 +0900

    lv1 - 5. JPA 의 이해 (JPQL)

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -17,6 +17,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class TodoService {
@@ -47,10 +49,10 @@ public class TodoService {
         );
     }
 
-    public Page<TodoResponse> getTodos(int page, int size) {
+    public Page<TodoResponse> getTodos(int page, int size, String weather, LocalDateTime updatedAfter, LocalDateTime updatedBefore) {
         Pageable pageable = PageRequest.of(page - 1, size);
 
-        Page<Todo> todos = todoRepository.findAllByOrderByModifiedAtDesc(pageable);
+        Page<Todo> todos = todoRepository.findAllWithParams(weather,updatedAfter,updatedBefore,pageable);
 
         return todos.map(todo -> new TodoResponse(
                 todo.getId(),
